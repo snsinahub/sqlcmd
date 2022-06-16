62b2fd0e649765c0328fd006b4a60f60ff089eec
Siavash Namvar <snsinahub@github.com>
2022-06-16 10:39:46 -0400

    add action file

---
+++ run_script.ps1
@@ -0,0 +1,19 @@
+<#
+
+#>
+param(
+    [Parameter (Mandatory)]
+    [string]$server,
+
+    [Parameter (Mandatory)]
+    [string]$dbName,
+
+    [Parameter (Mandatory)]
+    [string]$inputFile
+)
+
+echo $server
+echo $dbName
+echo $inputFile
+
+
